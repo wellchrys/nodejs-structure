0cff20e75e8f491354923ee44bb58512160ebd19
wellchrys <chrystiandejesus@hotmail.com>
2020-01-31 20:07:50 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+    development: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'nex2019@',
+            database: 'intranex'
+        }, 
+        pool: { min: 0, max: 7 },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: `${ __dirname }/src/database/migrations`
+        }
+    },
+
+    staging: {
+        client: 'mysql',
+        connection: {
+            database: 'my_db',
+            user:     'username',
+            password: 'password'
+        },
+        pool: { min: 2, max: 10 },
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: `${ __dirname }/src/database/migrations`
+    }
+},
+
+  production: {
+    client: 'mysql',
+    connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+    },
+    pool: { min: 2, max: 10 },
+
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${ __dirname }/src/database/migrations`
+    }
+  }
+
+};
---
+++ src/controllers/authController.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const User = require('');
+
+const router = express.Router();
+
+router.post('/register', async(req, res) => {
+    
+});
